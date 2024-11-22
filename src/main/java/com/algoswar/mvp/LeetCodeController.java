7cedde11653da2318fa4f831e2dfd56b6e11feb5
nellynguyen <yen225@lehigh.edu>
2024-11-21 23:05:48 -0500

    add openai as an attribute of leetcodecontroller

--- src/main/java/com/algoswar/mvp/LeetCodeController.java
+++ src/main/java/com/algoswar/mvp/LeetCodeController.java
@@ -1,12 +1,23 @@
 package com.algoswar.mvp;
 
+// External
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import java.util.*;
+// Internal
+import com.openai.api.service.OpenAiService;
 
 @RestController
 @RequestMapping("/api/leetcode")
-public class LeetCodeController {}
+public class LeetCodeController {
+
+    private final WebClient webClient;
+    private final OpenAiService openAiService;
+
+    @Autowired
+    public LeetCodeController(WebClient.Builder webClientBuilder, OpenAiService openAiService) {
+        this.webClient = webClientBuilder.baseUrl("https://alfa-leetcode-api.onrender.com").build();
+        this.openAiService = openAiService;
+    }
+}
